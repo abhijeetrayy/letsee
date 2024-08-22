6fa28ed18c7bf51213768a2b2c873eae5fab1701
Abhijeet Ray <abhijeet11ray@gmail.com>
2024-08-22 11:03:56 +0530

    favicon add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Unisocial",
-  description: "created by Uni Pvt",
+  title: "let's see - Unisocial",
+  description: "Cinema Social Media",
 };
 
 export default function RootLayout({
