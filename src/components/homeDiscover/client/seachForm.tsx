abef8e55248b803cafb012103fe604c0ce11a0c5
Abhijeet Ray <abhijeet11ray@gmail.com>
2024-08-24 23:14:03 +0530

    serach result - if not found

--- src/components/homeDiscover/client/seachForm.tsx
+++ src/components/homeDiscover/client/seachForm.tsx
@@ -84,137 +84,145 @@ const MovieSearch = () => {
           loading..
         </div>
       ) : (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
-          {results?.results?.map(
-            (data: any) =>
-              data?.media_type !== "person" && (
-                <div
-                  key={data.id}
-                  className=" overflow-hidden relative group flex flex-col  bg-black w-full text-gray-300 rounded-md   duration-300  lg:hover:scale-105 hover:z-30"
-                >
-                  <div className="absolute  top-0 left-0 flex flex-row justify-between w-full z-20">
-                    <p className="p-1 bg-black text-white rounded-br-md text-sm">
-                      {data.media_type}
-                    </p>
-                    {(data.release_date || data.first_air_date) && (
-                      <p className="p-1 bg-indigo-600 text-white rounded-bl-md text-sm">
-                        {new Date(data.release_date).getFullYear() ||
-                          new Date(data.first_air_date).getFullYear()}
+        <div>
+          {results.total_results == 0 && (
+            <div className="h-full w-full flex items-center justify-center">
+              Result for "{decodeURIComponent(query as string)}" is not found -
+              or check you spelling{" "}
+            </div>
+          )}
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3">
+            {results?.results?.map(
+              (data: any) =>
+                data?.media_type !== "person" && (
+                  <div
+                    key={data.id}
+                    className=" overflow-hidden relative group flex flex-col  bg-black w-full text-gray-300 rounded-md   duration-300  lg:hover:scale-105 hover:z-30"
+                  >
+                    <div className="absolute  top-0 left-0 flex flex-row justify-between w-full z-20">
+                      <p className="p-1 bg-black text-white rounded-br-md text-sm">
+                        {data.media_type}
                       </p>
-                    )}
-                  </div>
-                  <Link
-                    href={`/app/${data.media_type}/${data.id}--${(
-                      data.name || data.title
-                    )
-                      .trim()
-                      .replace(/[^a-zA-Z0-9]/g, "-")
-                      .replace(/-+/g, "-")}`}
-                    className=" h-full w-full"
-                  >
-                    <img
-                      className="relative object-cover w-full h-[230px] md:h-[300px] lg:h-full "
-                      src={
-                        data.poster_path || data.backdrop_path
-                          ? `https://image.tmdb.org/t/p/w342${
-                              data.poster_path || data.backdrop_path
-                            }`
-                          : "/no-photo.webp"
-                      }
-                      width={400}
-                      height={400}
-                      alt={data.title}
-                    />
-                  </Link>
-
-                  <div className="lg:absolute bottom-0 w-full bg-neutral-900 lg:opacity-0 lg:group-hover:opacity-100 ">
-                    <ThreePrefrenceBtn
-                      cardId={data.id}
-                      cardType={data.media_type}
-                      cardName={data.name || data.title}
-                      cardAdult={data.adult}
-                      cardImg={data.poster_path || data.backdrop_path}
-                    />
-
-                    <div
-                      title={data.name || data.title}
-                      className="w-full flex flex-col gap-2  px-4  bg-indigo-700  text-gray-200 "
+                      {(data.release_date || data.first_air_date) && (
+                        <p className="p-1 bg-indigo-600 text-white rounded-bl-md text-sm">
+                          {new Date(data.release_date).getFullYear() ||
+                            new Date(data.first_air_date).getFullYear()}
+                        </p>
+                      )}
+                    </div>
+                    <Link
+                      href={`/app/${data.media_type}/${data.id}--${(
+                        data.name || data.title
+                      )
+                        .trim()
+                        .replace(/[^a-zA-Z0-9]/g, "-")
+                        .replace(/-+/g, "-")}`}
+                      className=" h-full w-full"
                     >
-                      <Link
-                        href={`/app/${data.media_type}/${data.id}-${(
-                          data.name || data.title
-                        )
-                          .trim()
-                          .replace(/[^a-zA-Z0-9]/g, "-")
-                          .replace(/-+/g, "-")}}`}
-                        className="mb-1"
+                      <img
+                        className="relative object-cover w-full h-[230px] md:h-[300px] lg:h-full "
+                        src={
+                          data.poster_path || data.backdrop_path
+                            ? `https://image.tmdb.org/t/p/w342${
+                                data.poster_path || data.backdrop_path
+                              }`
+                            : "/no-photo.webp"
+                        }
+                        width={400}
+                        height={400}
+                        alt={data.title}
+                      />
+                    </Link>
+
+                    <div className="lg:absolute bottom-0 w-full bg-neutral-900 lg:opacity-0 lg:group-hover:opacity-100 ">
+                      <ThreePrefrenceBtn
+                        cardId={data.id}
+                        cardType={data.media_type}
+                        cardName={data.name || data.title}
+                        cardAdult={data.adult}
+                        cardImg={data.poster_path || data.backdrop_path}
+                      />
+
+                      <div
+                        title={data.name || data.title}
+                        className="w-full flex flex-col gap-2  px-4  bg-indigo-700  text-gray-200 "
                       >
-                        <span className="">
-                          {data?.title
-                            ? data.title.length > 20
-                              ? data.title?.slice(0, 20) + "..."
-                              : data.title
-                            : data.name.length > 20
-                            ? data.name?.slice(0, 20) + "..."
-                            : data.name}
-                        </span>
-                      </Link>
+                        <Link
+                          href={`/app/${data.media_type}/${data.id}-${(
+                            data.name || data.title
+                          )
+                            .trim()
+                            .replace(/[^a-zA-Z0-9]/g, "-")
+                            .replace(/-+/g, "-")}}`}
+                          className="mb-1"
+                        >
+                          <span className="">
+                            {data?.title
+                              ? data.title.length > 20
+                                ? data.title?.slice(0, 20) + "..."
+                                : data.title
+                              : data.name.length > 20
+                              ? data.name?.slice(0, 20) + "..."
+                              : data.name}
+                          </span>
+                        </Link>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-          )}
-          {results?.results?.map(
-            (data: any) =>
-              data?.media_type == "person" && (
-                <div
-                  key={data.id}
-                  className=" relative group flex flex-col   text-gray-300 rounded-md overflow-hidden duration-300 lg:hover:scale-105 hover:z-30"
-                >
-                  <div className="absolute top-0 left-0 z-40">
-                    <p className="p-1 bg-black text-white rounded-br-md text-sm">
-                      {data.media_type}
-                    </p>
-                  </div>
-                  <Link
-                    className="group-hover:underline"
-                    href={`/app/person/${data.id}-${data.name
-                      .trim()
-                      .replace(/[^a-zA-Z0-9]/g, "-")
-                      .replace(/-+/g, "-")}`}
+                )
+            )}
+            {results?.results?.map(
+              (data: any) =>
+                data?.media_type == "person" && (
+                  <div
+                    key={data.id}
+                    className=" relative group flex flex-col   text-gray-300 rounded-md overflow-hidden duration-300 lg:hover:scale-105 hover:z-30"
                   >
-                    <img
-                      className="relative rounded-md object-cover h-[230px] md:h-[300px] w-full  "
-                      src={
-                        data.profile_path
-                          ? `https://image.tmdb.org/t/p/h632${data.profile_path}`
-                          : "/no-photo.webp"
-                      }
-                      width={400}
-                      height={400}
-                      alt={data.title}
-                    />
-                  </Link>
-                  <span className=" flex flex-col gap-3  py-2 hlimitSearch px-4 h-full bg-indigo-700">
-                    <div className="mb-1">
-                      <Link
-                        className="group-hover:underline"
-                        href={`/app/person/${data.id}-${data.name
-                          .trim()
-                          .replace(/[^a-zA-Z0-9]/g, "-")
-                          .replace(/-+/g, "-")}`}
-                      >
-                        {data.name}
-                      </Link>
+                    <div className="absolute top-0 left-0 z-40">
+                      <p className="p-1 bg-black text-white rounded-br-md text-sm">
+                        {data.media_type}
+                      </p>
                     </div>
-                    <p className="text-xs mb-1 ">
-                      {data.release_date || data.first_air_date}
-                    </p>
-                    <p className=" text-xs ">{data.known_for_department}</p>
-                  </span>
-                </div>
-              )
-          )}
+                    <Link
+                      className="group-hover:underline"
+                      href={`/app/person/${data.id}-${data.name
+                        .trim()
+                        .replace(/[^a-zA-Z0-9]/g, "-")
+                        .replace(/-+/g, "-")}`}
+                    >
+                      <img
+                        className="relative rounded-md object-cover h-[230px] md:h-[300px] w-full  "
+                        src={
+                          data.profile_path
+                            ? `https://image.tmdb.org/t/p/h632${data.profile_path}`
+                            : "/no-photo.webp"
+                        }
+                        width={400}
+                        height={400}
+                        alt={data.title}
+                      />
+                    </Link>
+                    <span className=" flex flex-col gap-3  py-2 hlimitSearch px-4 h-full bg-indigo-700">
+                      <div className="mb-1">
+                        <Link
+                          className="group-hover:underline"
+                          href={`/app/person/${data.id}-${data.name
+                            .trim()
+                            .replace(/[^a-zA-Z0-9]/g, "-")
+                            .replace(/-+/g, "-")}`}
+                        >
+                          {data.name}
+                        </Link>
+                      </div>
+                      <p className="text-xs mb-1 ">
+                        {data.release_date || data.first_air_date}
+                      </p>
+                      <p className=" text-xs ">{data.known_for_department}</p>
+                    </span>
+                  </div>
+                )
+            )}
+          </div>
         </div>
       )}
 
