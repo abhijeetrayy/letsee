743e3d9383be98c7bdf486cd8adcaa59598b1f10
Abhijeet Ray <abhijeet11ray@gmail.com>
2025-02-20 19:53:26 +0530

    content tile on messaging app like whatsapp

--- src/app/app/movie/[id]/page.tsx
+++ src/app/app/movie/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Countrydata } from "@/staticData/countryName";
 import Movie from "@components/clientComponent/movie";
+import Head from "next/head";
 
 type PageProps = {
   params: Promise<{ id: string }>;
@@ -54,18 +55,47 @@ const MovieDetails = async ({ params }: PageProps) => {
     Countrydata.filter((item: any) => item.iso_3166_1 == name)
   );
 
+  const movieTitle = movie.name || "Movie Details";
+  const movieOverview = movie.overview || "Check out this movie!";
+  const movieImage = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "/default-movie.jpg";
+  const movieUrl = `https://letsee-dusky.vercel.app/app/movie/${id}`; // Change this to your actual domain
+
   return (
-    <div>
-      <Movie
-        CountryName={CountryName}
-        videos={videos}
-        Pimages={Pimages}
-        Bimages={Bimages}
-        movie={movie}
-        credits={credits}
-        id={id}
-      />
-    </div>
+    <>
+      {/* Open Graph & Twitter Meta Tags */}
+      <Head>
+        <title>{movieTitle} - Watch Details</title>
+        <meta name="description" content={movieOverview} />
+
+        {/* Open Graph Meta Tags (for WhatsApp, Facebook, LinkedIn) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={movieUrl} />
+        <meta property="og:title" content={movieTitle} />
+        <meta property="og:description" content={movieOverview} />
+        <meta property="og:image" content={movieImage} />
+        <meta property="og:image:alt" content={movieTitle} />
+
+        {/* Twitter Card Meta Tags (for Twitter, Telegram) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content={movieUrl} />
+        <meta name="twitter:title" content={movieTitle} />
+        <meta name="twitter:description" content={movieOverview} />
+        <meta name="twitter:image" content={movieImage} />
+      </Head>
+      <div>
+        <Movie
+          CountryName={CountryName}
+          videos={videos}
+          Pimages={Pimages}
+          Bimages={Bimages}
+          movie={movie}
+          credits={credits}
+          id={id}
+        />
+      </div>
+    </>
   );
 };
 
