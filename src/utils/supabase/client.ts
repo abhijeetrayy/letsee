e142dd69ec190c9994ce99887da94e5d6c5d309c
Abhijeet Ray <abhijeet11ray@gmail.com>
2025-03-02 04:05:31 +0530

    middleware update-4

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,15 +1,8 @@
-import { createClient as createSupabaseClient } from "@supabase/supabase-js";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const createClient = () => {
-  return createSupabaseClient(
+export function createClient() {
+  return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        autoRefreshToken: true,
-        persistSession: true,
-        detectSessionInUrl: true, // Important for reset links
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
-};
+}
