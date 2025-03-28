2439daebd587faa98b00fea0f92df1550fa1ab66
Abhijeet Ray <abhijeet11ray@gmail.com>
2025-03-28 14:00:34 +0530

    client supabase to api 3 more

--- src/app/api/recommendations/route.ts
+++ src/app/api/recommendations/route.ts
@@ -1,12 +1,8 @@
+import { createClient } from "@/utils/supabase/server";
 import { NextResponse } from "next/server";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
-);
 
 export async function POST(request: Request) {
+  const supabase = await createClient();
   const url = new URL(request.url);
   const path = url.pathname;
 
